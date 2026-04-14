99cd2fa6f4d9c9a072f3c9a9e690963207bceb62
nhs505 <nhsadm505@gmail.com>
2026-04-14 09:36:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,25 @@ let questions = [];
 let current = 0;
 let score = 0;
 
-fetch('questions.json')
-  .then(res => res.json())
-  .then(data => {
-    questions = data;
-    showQuestion();
-  });
+window.onload = function () {
+  fetch('questions.json')
+    .then(res => res.json())
+    .then(data => {
+      questions = data;
+      showQuestion();
+    })
+    .catch(err => {
+      document.getElementById("question").innerText =
+        "Error loading questions.json";
+      console.log(err);
+    });
+};
 
 function showQuestion() {
+  if (!questions.length) return;
+
   let q = questions[current];
+
   document.getElementById("question").innerText = q.question;
 
   let choicesDiv = document.getElementById("choices");
@@ -19,25 +29,27 @@ function showQuestion() {
   q.choices.forEach(choice => {
     let btn = document.createElement("button");
     btn.innerText = choice;
-    btn.onclick = () => checkAnswer(choice);
+
+    btn.onclick = function () {
+      if (choice === q.answer) {
+        score++;
+        alert("Correct!");
+      } else {
+        alert("Wrong!");
+      }
+    };
+
     choicesDiv.appendChild(btn);
   });
 }
 
-function checkAnswer(choice) {
-  if (choice === questions[current].answer) {
-    score++;
-    alert("Correct!");
-  } else {
-    alert("Wrong!");
-  }
-}
-
 function nextQuestion() {
   current++;
+
   if (current < questions.length) {
     showQuestion();
   } else {
-    document.body.innerHTML = `<h1>Score: ${score}</h1>`;
+    document.body.innerHTML =
+      `<h1>Score: ${score}/${questions.length}</h1>`;
   }
 }
